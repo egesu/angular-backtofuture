f5c1cdbeb12faa7e15f8818603c52e8fecb2ff12
Ege Sertçetin <ege@sertcetin.com>
2015-10-21 14:46:13 +0300

    init

---
+++ backtofuture.js
@@ -0,0 +1,19 @@
+angular.module('backtofuture', [])
+    .directive('martin', [
+        '$interval',
+        function($interval) {
+            return {
+                link: function(scope, element, attrs) {
+                    $interval(function() {
+                        $(element).addClass('delorean');
+                        $(element).css('left', '1500px').show();
+                        $(element).animate({
+                            left: '-250px'
+                        }, 2000, 'swing', function() {
+                            $(element).hide();
+                        });
+                    }, 5000);
+                }
+            };
+        }
+    ]);
